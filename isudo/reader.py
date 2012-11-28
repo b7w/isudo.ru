396028ab86c9aa84d1904d4c694c2af9cb2c8432
B7W <isudoblog@gmail.com>
2012-11-28 13:26:18 +0400

    Remove macros class. Add Resource instead.
    Now no amy image, or icon. Just "(~relative/path)". Plus "[left|center]"

--- isudo/reader.py
+++ isudo/reader.py
@@ -7,46 +7,29 @@ from isudo.post import Meta, Post
 from isudo.utils import url
 
 
-class Macros:
+class Resource:
+    RE = re.compile('\(~[\w\./-]+\)')
+
     def __init__(self, text, post):
         """
         :type text: str
         :type post: blog.post.Post
         """
-        self.md = text
+        self.text = text
         self.post = post
-        name, body = text[:-1].split('{')
-        self.name = name.strip()
-        self.body = body.strip()
+        self.body = text[2:-1].strip()
 
-    def render(self, post):
-        raise NotImplementedError()
+    def render(self):
+        return '(%s)' % url(self.post.url, self.body, last=False)
 
     def replace(self, text):
-        return text.replace(self.md, self.render(self.post))
+        return text.replace(self.text, self.render())
 
     def __repr__(self):
-        return 'Macros{%s, %s}' % (self.name, self.body)
-
-
-class IconMacros(Macros):
-    def render(self, post):
-        t = """<a href="{url}"><img src="{image}" class="align-left"/></a>"""
-        image = url(post.url, self.body, last=False)
-        return t.format(url=post.url, image=image)
-
-
-class ImageMacros(Macros):
-    def render(self, post):
-        t = """<a href="{image}"><img src="{image}" class="align-center"/></a>"""
-        image = url(post.url, self.body, last=False)
-        return t.format(image=image)
+        return 'Resource{%s}' % self.body
 
 
 class Reader:
-    ICON = re.compile('icon\{\s*[\w\./-]+\s*}')
-    IMAGE = re.compile('image\{\s*[\w\./-]+\s*}')
-
     def read(self, path):
         with open(path, encoding='utf8') as f:
             raw = f.read().strip()
@@ -56,14 +39,5 @@ class Reader:
         meta = Meta.fromDict(yaml.load(meta))
 
         post = Post(path, meta, text.strip())
-        post.resources = self.find_macros(text, post)
+        post.resources = [Resource(i, post) for i in Resource.RE.findall(text)]
         return post
-
-    def find_macros(self, text, post):
-        """
-        :type text: str
-        :rtype: str
-        """
-        macros = [IconMacros(i, post) for i in self.ICON.findall(text)]
-        macros.extend(ImageMacros(i, post) for i in self.IMAGE.findall(text))
-        return macros
