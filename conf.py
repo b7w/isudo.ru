fee00ad8dfd25d8c66c57d939bfa048e4a9378b3
B7W <isudoblog@gmail.com>
2013-01-03 13:22:03 +0400

    Add extra html in template to set yandex/google counters

--- conf.py
+++ conf.py
@@ -8,8 +8,10 @@ BLOG_TITLE = 'Black&White’s Blog'
 
 BLOG_DESCRIPTION = 'Personal blog'
 
-BLOG_KEYWORDS = 'ubuntu,homeServer,torrent,programming,python,howto'
+BLOG_KEYWORDS = 'ubuntu,home server,torrent,programming,python,howto'
 
 GOOGLE_PLUS = '114553589811306443494'
 
 DOMAIN_SUB_FOLDER = ''
+
+EXTRA_HTML = """<!--Yandex.Metrika counter--><script type="text/javascript">(function(d,w,c){(w[c]=w[c]||[]).push(function(){try{w.yaCounter9952873=new Ya.Metrika({id:9952873,webvisor:true,clickmap:true,trackLinks:true,accurateTrackBounce:true});}catch(e){}});var n=d.getElementsByTagName("script")[0],s=d.createElement("script"),f=function(){n.parentNode.insertBefore(s,n);};s.type="text/javascript";s.async=true;s.src=(d.location.protocol=="https:"?"https:":"http:")+"//mc.yandex.ru/metrika/watch.js";if(w.opera=="[object Opera]"){d.addEventListener("DOMContentLoaded",f,false);}else{f();}})(document,window,"yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/9952873"style="position:absolute; left:-9999px;"alt=""/></div></noscript><!--/Yandex.Metrika counter-->"""
