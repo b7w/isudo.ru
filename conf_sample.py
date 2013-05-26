8bb07266a3e96f4aed7f488503ab24940f86507d
B7W <isudoblog@gmail.com>
2013-05-26 12:58:42 +0400

    Remove conf add sample

---
+++ conf_sample.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+TIME_ZONE = 'Europe/Moscow'
+
+BLOG_URL = 'http://isudo.ru'
+
+BLOG_TITLE = 'Black&White’s Blog'
+
+BLOG_DESCRIPTION = 'Personal B7W blog'
+
+BLOG_KEYWORDS = 'ubuntu,home server,torrent,programming,python,howto'
+
+GOOGLE_PLUS = '114553589811306443494'
+
+DOMAIN_SUB_FOLDER = ''
+
+LINKS = [
+    ('/about/', 'Информация о ресурсе и авторе', 'О сайте'),
+    ('https://plus.google.com/114553589811306443494', 'Follow Me', 'Google plus'),
+]
